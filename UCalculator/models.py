87ecd417b1299465772d7e4206d2d9ce9b31f834
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2023-01-27 20:42:35 +0000

    added basic models

--- UCalculator/models.py
+++ UCalculator/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Material(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    thermal_conductivity = models.FloatField(max_length=10)
+
+class Composite(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+class Component(models.Model):
+    thickness = models.IntegerField(max_length=10)
+    material = models.ForeignKey(Material)
+    composite = models.ForeignKey(Composite)
