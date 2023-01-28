19d84c341c474e84fe06393e98c0763a0e81f38a
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2023-01-28 12:10:00 +0000

    added basic calculator page tests

--- UCalculator/tests.py
+++ UCalculator/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from .views import home
+from .views import home, calculator
 
 
 # Create your tests here.
@@ -13,3 +13,13 @@ class HomeTests(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+class CalculatorTests(TestCase):
+    def test_calculator_view_status_code(self):
+        url = reverse('calculator')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+      
+    def test_calculator_url_resolves_calculator_view(self):
+        view = resolve('/calculator')
+        self.assertEquals(view.func, calculator)
