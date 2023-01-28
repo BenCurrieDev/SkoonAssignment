1acfeec10a7b1f710eb611bdbbf1c47f812f9cc6
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2023-01-28 10:17:03 +0000

    added materials to db and printed to home page

--- UCalculator/models.py
+++ UCalculator/models.py
@@ -4,11 +4,17 @@ from django.db import models
 class Material(models.Model):
     name = models.CharField(max_length=30, unique=True)
     thermal_conductivity = models.FloatField(max_length=10)
+    def __str__(self):
+        return self.name
 
 class Composite(models.Model):
     name = models.CharField(max_length=30, unique=True)
+    def __str__(self):
+        return self.name
 
 class Component(models.Model):
     thickness = models.IntegerField()
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     composite = models.ForeignKey(Composite, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.material.name + ' (' + self.thickness + 'mm)'
