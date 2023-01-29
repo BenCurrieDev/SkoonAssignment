91b047f213114a67e3b7b34cfff9e01051fda172
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2023-01-29 15:28:15 +0000

    added signup page

--- SkoonAssignment/urls.py
+++ SkoonAssignment/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 from UCalculator import views
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('signup/', accounts_views.signup, name="signup"),
     path('calculator', views.calculator, name="calculator"),
 ]
