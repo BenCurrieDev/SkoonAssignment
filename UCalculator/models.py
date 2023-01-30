69e137429faedcee9d405df88ee909f020fa0a0f
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2023-01-30 17:47:23 +0000

    added color to material model

--- UCalculator/models.py
+++ UCalculator/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Material(models.Model):
     name = models.CharField(max_length=30, unique=True)
     thermal_conductivity = models.FloatField()
+    color = models.CharField(max_length=30, default="grey")
     def __str__(self):
         return self.name
 
