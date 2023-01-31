438be4d1eda559b10686f217f703d7b6aa58bbf3
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2023-01-31 14:39:36 +0000

    switched calculator to home page

--- SkoonAssignment/urls.py
+++ SkoonAssignment/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('signup/', accounts_views.signup, name="signup"),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-    path('calculator', views.calculator, name="calculator"),
+    path('database', views.database, name="database"),
 ]
