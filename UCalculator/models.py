31ef8252162cfb698187cb9783d855aca55460fd
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2023-02-04 14:52:54 +0000

    removed unique from composite names

--- UCalculator/models.py
+++ UCalculator/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Material(models.Model):
     name = models.CharField(max_length=30, unique=True)
     thermal_conductivity = models.FloatField()
     color = models.CharField(max_length=30, default="grey")
+
     def __str__(self):
         return self.name
 
+
 class Composite(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=30)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
 
@@ -30,19 +32,20 @@ class Composite(models.Model):
     def __str__(self):
         return self.name
 
+
 class Component(models.Model):
     thickness = models.IntegerField()
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
-    composite = models.ForeignKey(Composite, on_delete=models.CASCADE, blank=True, null=True)
+    composite = models.ForeignKey(
+        Composite, on_delete=models.CASCADE, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
 
     def calcR(self):
         return ((self.thickness * 0.001) / self.material.thermal_conductivity)
-    
+
     def displayR(self):
         return round((self.thickness * 0.001) / self.material.thermal_conductivity, 2)
 
     def __str__(self):
         return self.material.name + ' (' + str(self.thickness) + 'mm)'
-
